9d165d2aa273e2dcc2cccce87ec99d9fe939ec9e
olchia <olyaburtseva@me.com>
2025-10-23 00:11:16 +0300

    Обновила Pie-график и исправила предупреждения ESLint

--- src/components/AnalyticsDashboard.tsx
+++ src/components/AnalyticsDashboard.tsx
@@ -222,7 +222,7 @@ export function AnalyticsDashboard() {
                   cx="50%"
                   cy="50%"
                   labelLine={false}
-                  label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
+                  label={({ name, percent }) => {if (typeof percent !== 'number') return name; return `${name} ${(percent * 100).toFixed(0)}%`;}}
                   outerRadius={80}
                   fill="#8884d8"
                   dataKey="value"
